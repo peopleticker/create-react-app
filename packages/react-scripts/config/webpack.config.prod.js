de859e8fe0e7fbffb4f4784a035d0d72a02b46fe
Chris Portela <cportela@peopleticker.com>
2017-08-22 16:27:48 -0400

    Added sass

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -196,7 +196,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.s?css$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -230,6 +230,12 @@ module.exports = {
                         ],
                       },
                     },
+                    {
+                      loader: require.resolve('sass-loader'),
+                      options: {
+                        sourceMap: shouldUseSourceMap,
+                      }
+                    }
                   ],
                 },
                 extractTextPluginOptions
